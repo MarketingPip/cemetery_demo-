a6bcfd63cb365c566f98832a7c86a845e0659ad1
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-04-03 16:50:36 -0400

    Update file_creation_date.rb

--- _plugins/file_creation_date.rb
+++ _plugins/file_creation_date.rb
@@ -1,13 +1,18 @@
 # _plugins/file_creation_date.rb
+
 module Jekyll
-  module FileCreationDate
-    # Create a custom filter to get the creation date
-    def file_creation_date(input)
-      file_path = File.join(Dir.pwd, input)  # Get the full file path
-      return File.birthtime(file_path) rescue nil  # Return the creation date, or nil if it fails
+  class Post
+    def file_creation_date
+      # Get the absolute path of the file
+      file_path = File.join(Jekyll.sites[0].source, self.path)
+
+      # Ensure the file exists and retrieve its creation time
+      if File.exist?(file_path)
+        timestamp = File.ctime(file_path)  # Use ctime (creation time)
+        timestamp.strftime('%a, %d %b %Y %H:%M:%S +0000')  # Format it to RFC-822
+      else
+        "Sat, 01 Jan 2000 00:00:00 +0000"  # Default in case of error
+      end
     end
   end
 end
-
-# Register the filter with Jekyll
-Liquid::Template.registerFilter(Jekyll::FileCreationDate)
