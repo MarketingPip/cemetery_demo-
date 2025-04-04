7123a01d97299b6f19ec01af04ce9920466230e2
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-04-04 16:28:30 -0400

    Update auto_delete_expired_posts.rb

--- _plugins/auto_delete_expired_posts.rb
+++ _plugins/auto_delete_expired_posts.rb
@@ -7,13 +7,26 @@ module Jekyll
       site.posts.docs.each do |post|
         # Check if the post has an expire_date
         if post.data['expire_date']
-          expire_date = Date.parse(post.data['expire_date'])
+          begin
+            # Ensure expire_date is a Date object, even if it's stored as a string
+            expire_date = case post.data['expire_date']
+                          when String
+                            Date.parse(post.data['expire_date'])
+                          when Date
+                            post.data['expire_date']
+                          else
+                            nil
+                          end
 
-          # If the expire_date is in the past, delete the post from the site
-          if expire_date < Date.today
-            # Remove the post from the collection
-            site.posts.docs.delete(post)
-            puts "Post '#{post.data['title']}' has expired and has been deleted."
+            # If the expire_date is valid and in the past, delete the post
+            if expire_date && expire_date < Date.today
+              # Remove the post from the collection
+              site.posts.docs.delete(post)
+              puts "Post '#{post.data['title']}' has expired and has been deleted."
+            end
+          rescue ArgumentError => e
+            # Handle invalid date format
+            puts "Invalid expire_date format for post '#{post.data['title']}': #{e.message}"
           end
         end
       end
