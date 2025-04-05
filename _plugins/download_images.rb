c2f8ad73b57d6ac1a9f5061e0f3558aeaa4a7073
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-04-04 21:23:13 -0400

    Create download_images.rb

---
+++ _plugins/download_images.rb
@@ -0,0 +1,51 @@
+# _plugins/download_images.rb
+
+require 'open-uri'
+require 'fileutils'
+require 'uri'
+
+module Jekyll
+  class DownloadImages < Generator
+    priority :low
+
+    def generate(site)
+      # Get the download folder path from the configuration
+      download_folder = site.config['download_images_folder'] || 'assets/images'
+
+      # Create the download folder if it doesn't exist
+      FileUtils.mkdir_p(download_folder)
+
+      # Loop through all pages/posts in the site and find external image links
+      site.pages.each { |page| download_external_images(page, download_folder) }
+      site.posts.each { |post| download_external_images(post, download_folder) }
+    end
+
+    def download_external_images(item, download_folder)
+      # Regex to match all image URLs
+      image_urls = item.content.scan(/(?:src|href)="(https?:\/\/(?:[a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,})(\/[^\"]+\.(?:jpg|jpeg|png|gif|svg))"/)
+
+      # Download and save each image
+      image_urls.each do |url|
+        image_url = url[0] + url[1]
+        file_name = File.basename(URI.parse(image_url).path)
+        download_path = File.join(Dir.pwd, download_folder, file_name)
+
+        unless File.exist?(download_path)
+          puts "Downloading #{image_url} to #{download_path}"
+          begin
+            open(image_url) do |image|
+              File.open(download_path, 'wb') do |f|
+                f.write(image.read)
+              end
+            end
+          rescue StandardError => e
+            puts "Failed to download #{image_url}: #{e.message}"
+          end
+        end
+
+        # Replace the image URL with the path to the downloaded image
+        item.content.gsub!(image_url, File.join(download_folder, file_name))
+      end
+    end
+  end
+end
