3b7ef1c70c9e62621df7c0976996a63a9ef400d0
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-04-05 15:54:26 -0400

    Update update-authors.js

--- .github/workflows/updateAuthors/update-authors.js
+++ .github/workflows/updateAuthors/update-authors.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import * as matter from 'gray-matter';
+import matter from 'gray-matter';
 
 // Directory where your posts and author files are located
 const postsDir = path.join(process.cwd(), '_posts');
