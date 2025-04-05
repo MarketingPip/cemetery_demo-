12b2db918fe95d738963549814d81c25258e7db5
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-04-04 23:25:22 -0400

    Update compile_tags.rb

--- _plugins/compile_tags.rb
+++ _plugins/compile_tags.rb
@@ -1,37 +1,43 @@
 # Filename: _plugins/compile_tags.rb
 require 'fileutils'
 
-Jekyll::Hooks.register :posts, :post_write do
-  # Directory to save tag pages
-  TAG_DIR = '_tag'  # Folder for tag pages
+module Jekyll
+  class GenerateTagPages < Generator
+    safe true
 
-  # Ensure the tag directory exists
-  FileUtils.mkdir_p(TAG_DIR) unless File.exists?(TAG_DIR)
+    def generate(site)
+      # Directory to save tag pages
+      TAG_DIR = '_tag'  # Folder for tag pages
 
-  # Collect all tags from posts
-  all_tags = []
-  Jekyll.site.posts.docs.each do |post|
-    tags = post.data['tags'] || []
-    all_tags += tags
-  end
+      # Ensure the tag directory exists
+      FileUtils.mkdir_p(TAG_DIR) unless File.exists?(TAG_DIR)
 
-  # Remove duplicate tags
-  all_tags = all_tags.uniq
+      # Collect all tags from posts
+      all_tags = []
+      site.posts.docs.each do |post|
+        tags = post.data['tags'] || []
+        all_tags += tags
+      end
 
-  # Create a tag page for each tag
-  all_tags.each do |tag|
-    tag_file_path = File.join(TAG_DIR, "#{tag}.md")
-    
-    # Only create the tag page if it doesn't exist already
-    unless File.exist?(tag_file_path)
-      # Write the tag page with front matter
-      File.open(tag_file_path, 'w') do |f|
-        f.write("---\n")
-        f.write("layout: tagpage\n")
-        f.write("tag: #{tag}\n")
-        f.write("robots: noindex\n")
-        f.write("---\n\n")
-        f.write("This is the tag page for `#{tag}`.\n")
+      # Remove duplicate tags
+      all_tags = all_tags.uniq
+
+      # Create a tag page for each tag
+      all_tags.each do |tag|
+        tag_file_path = File.join(TAG_DIR, "#{tag}.md")
+        
+        # Only create the tag page if it doesn't exist already
+        unless File.exist?(tag_file_path)
+          # Write the tag page with front matter
+          File.open(tag_file_path, 'w') do |f|
+            f.write("---\n")
+            f.write("layout: tagpage\n")
+            f.write("tag: #{tag}\n")
+            f.write("robots: noindex\n")
+            f.write("---\n\n")
+            f.write("This is the tag page for `#{tag}`.\n")
+          end
+        end
       end
     end
   end
