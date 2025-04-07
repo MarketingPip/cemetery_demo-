b6cd943c01e32154e136c8ab4db3356f82a2cf9e
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-04-07 17:05:19 -0400

    Update expire_posts.rb

--- _plugins/expire_posts.rb
+++ _plugins/expire_posts.rb
@@ -1,12 +1,14 @@
 module Jekyll
   class ExpirePosts < Generator
     safe true
+    priority :low # Set priority to low so it runs earlier
 
-    Jekyll::Hooks.register :site, :pre_render do |site|
-      # Ensure expired posts are handled before rendering pages
+    def generate(site)
+      # Iterate over the posts in the site and reject expired ones
       site.posts.docs.reject! do |post|
         if post.data['expire_date']
           begin
+            # Ensure expire_date is a Date object, even if it's stored as a string
             expire_date = case post.data['expire_date']
                           when String
                             Date.parse(post.data['expire_date'])
@@ -15,6 +17,8 @@ module Jekyll
                           else
                             nil
                           end
+
+            # If expire_date is valid and in the past, remove the post from the collection
             if expire_date && expire_date < Date.today
               post.data['published'] = false
               puts "Post '#{post.data['title']}' has expired and will not be rendered."
