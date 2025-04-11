0096f7ac9219ca754dd65a991df9c4420f49f5bc
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-04-11 19:42:12 -0400

    Update sw.js

--- assets/js/pwa/sw.js
+++ assets/js/pwa/sw.js
@@ -70,6 +70,53 @@ if (workbox) {
         importScripts(PWA_CONFIG.cache.custom.script);
     }
 
+
+
+ const CACHE = "pwabuilder-page";
+
+
+const offlineFallbackPage = PWA_CONFIG.sw.offline_route;
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
+
+    
 } else {
     console.error('PWA: Workbox didn\'t load.');
 }
