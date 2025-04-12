96367a5b477344cd0f596247e5c990a1a361c498
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-04-11 20:48:30 -0400

    Update sw.js

--- assets/js/pwa/sw.js
+++ assets/js/pwa/sw.js
@@ -72,21 +72,15 @@ if (workbox) {
 
 
 
- const CACHE = "pwabuilder-page";
-
 
 const offlineFallbackPage = PWA_CONFIG.sw.offline_route;
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
 
-self.addEventListener('install', async (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+    caches.open("static-cache").then((cache) => {
+      return cache.addAll([offlineFallbackPage]);
+    })
   );
 });
 
@@ -94,26 +88,10 @@ if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(offlineFallbackPage))
+  );
 });
 
     
