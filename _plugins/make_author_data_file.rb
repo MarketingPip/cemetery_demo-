586a7f07c4a7fb2b93c68f8ecc09adcdf098ceaa
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-05-09 07:45:07 -0400

    Update make_author_data_file.rb

--- _plugins/make_author_data_file.rb
+++ _plugins/make_author_data_file.rb
@@ -1,35 +1,37 @@
-# Defining a Jekyll generator plugin to process author files and create a data file
+require 'yaml'
+
 module Jekyll
   class AuthorDataGenerator < Generator
     safe true
     priority :normal
 
     def generate(site)
-      # Initializing an empty hash to store author data
       author_data = {}
-
-      # Finding all files in the _authors directory
       author_files = Dir.glob(File.join(site.source, '_authors', '*.{md,markdown}'))
 
       author_files.each do |file_path|
-        # Reading the front matter of each author file
-        front_matter = YAML.load_file(file_path)
-        next unless front_matter && front_matter['author']
+        begin
+          # Use Psych.safe_load with allowed classes to avoid DisallowedClass errors
+          front_matter = YAML.safe_load(File.read(file_path), permitted_classes: [String, Hash, Array])
+          next unless front_matter && front_matter['author']
 
-        author_id = front_matter['author']
-        author_data[author_id] = {
-          'name' => front_matter['name'],
-          'role' => front_matter['role'],
-          'image' => front_matter['image'],
-          'social' => front_matter['social'] || {
-            'facebook' => '#',
-            'twitter' => '#',
-            'linkedin' => '#'
+          author_id = front_matter['author']
+          author_data[author_id] = {
+            'name' => front_matter['name'],
+            'role' => front_matter['role'],
+            'image' => front_matter['image'],
+            'social' => front_matter['social'] || {
+              'facebook' => '#',
+              'twitter' => '#',
+              'linkedin' => '#'
+            }
           }
-        }
+        rescue Psych::DisallowedClass, Psych::SyntaxError => e
+          Jekyll.logger.warn "Skipping author file #{file_path}: #{e.message}"
+          next
+        end
       end
 
-      # Writing the author data to _data/authors.yml
       output_path = File.join(site.source, '_data', 'authors.yml')
       FileUtils.mkdir_p(File.dirname(output_path))
       File.write(output_path, author_data.to_yaml)
