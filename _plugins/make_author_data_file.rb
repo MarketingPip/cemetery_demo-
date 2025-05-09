4c39fa149eefbfa3d837387ef70ce7cff51dd76f
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-05-09 08:06:50 -0400

    Update make_author_data_file.rb

--- _plugins/make_author_data_file.rb
+++ _plugins/make_author_data_file.rb
@@ -4,61 +4,56 @@ require 'fileutils'
 module Jekyll
   class AuthorDataGenerator < Generator
     safe true
-    priority :high # Run early in the build process
+    priority :high
 
     def generate(site)
+      Jekyll.logger.info "AuthorDataGenerator: Starting generation"
       author_data = {}
       author_files = Dir.glob(File.join(site.source, '_authors', '*.{md,markdown}'))
+      Jekyll.logger.info "AuthorDataGenerator: Found #{author_files.length} author files"
 
       author_files.each do |file_path|
+        Jekyll.logger.info "AuthorDataGenerator: Processing #{file_path}"
         begin
           front_matter = YAML.safe_load(File.read(file_path), permitted_classes: [String, Hash, Array])
-          next unless front_matter && front_matter['author']
-
-          author_id = front_matter['author']
-          author_data[author_id] = {
-            'name' => front_matter['name'],
-            'role' => front_matter['role'],
-            'image' => front_matter['image'],
-            'social' => front_matter['social'] || {
-              'facebook' => '#',
-              'twitter' => '#',
-              'linkedin' => '#'
+          if front_matter && front_matter['author']
+            author_id = front_matter['author']
+            Jekyll.logger.info "AuthorDataGenerator: Found author ID '#{author_id}'"
+            author_data[author_id] = {
+              'name' => front_matter['name'],
+              'role' => front_matter['role'],
+              'image' => front_matter['image'],
+              'social' => front_matter['social'] || {
+                'facebook' => '#',
+                'twitter' => '#',
+                'linkedin' => '#'
+              }
             }
-          }
+          else
+            Jekyll.logger.warn "AuthorDataGenerator: No 'author' key in #{file_path}"
+          end
         rescue Psych::DisallowedClass, Psych::SyntaxError => e
-          Jekyll.logger.warn "Skipping author file #{file_path}: #{e.message}"
-          next
+          Jekyll.logger.warn "AuthorDataGenerator: Skipping #{file_path}: #{e.message}"
         end
       end
 
-      # Write to source _data directory for template access
       source_output_path = File.join(site.source, '_data', 'authors.yml')
       FileUtils.mkdir_p(File.dirname(source_output_path))
       File.write(source_output_path, author_data.to_yaml)
-
-      # Register _data/authors.yml as a static file to be copied to _site/data/authors.yml
-      site.static_files << AuthorStaticFile.new(
-        site,
-        site.source,
-        '_data',
-        'authors.yml',
-        'data/authors.yml'
-      )
+      Jekyll.logger.info "AuthorDataGenerator: Wrote authors.yml to #{source_output_path}"
     end
   end
+end
 
-  # Custom StaticFile class to copy _data/authors.yml to _site/data/authors.yml
-  class AuthorStaticFile < StaticFile
-    def initialize(site, base, dir, name, destination)
-      super(site, base, dir, name)
-      @relative_path = File.join(dir, name)
-      @destination_path = destination
-    end
-
-    # Override destination to place the file in _site/data/authors.yml
-    def destination(dest)
-      File.join(dest, @destination_path)
-    end
+# Hook to copy _data/authors.yml to _site/data/authors.yml after build
+Jekyll::Hooks.register :site, :post_write do |site|
+  source_path = File.join(site.source, '_data', 'authors.yml')
+  dest_path = File.join(site.dest, 'data', 'authors.yml')
+  if File.exist?(source_path)
+    FileUtils.mkdir_p(File.dirname(dest_path))
+    FileUtils.cp(source_path, dest_path)
+    Jekyll.logger.info "AuthorDataGenerator: Copied #{source_path} to #{dest_path}"
+  else
+    Jekyll.logger.warn "AuthorDataGenerator: Source file #{source_path} not found"
   end
 end
