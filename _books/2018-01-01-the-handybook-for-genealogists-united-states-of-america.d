dcaca8daa9144b36b57ae2d4149557615f907320
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-06-11 22:03:33 -0400

    Create  2018-01-01-the-handybook-for-genealogists-united-states-of-america.d

---
+++ _books/2018-01-01-the-handybook-for-genealogists-united-states-of-america.d
@@ -0,0 +1,9 @@
+---
+layout: review
+title: "The Handybook for Genealogists: United States of America"
+author: George B. Everton
+year: "2006"
+pdfFile: https://archive.org/download/handybookforgene00ilever/handybookforgene00ilever.pdf
+olid: "OL17197453M"
+---
+Published in 2006, *The Handybook for Genealogists: United States of America* by George B. Everton is a must-have reference for genealogists tracing their American roots. This comprehensive guide offers detailed information about U.S. records, archives, libraries, and resources available for genealogical research. With updates on the latest records and resources, Everton’s handbook is an invaluable tool for anyone researching their American ancestry.
