a4e8143b93c1bb3dbc63001364fab2ed51718e24
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-09-07 03:29:04 -0400

    Update custom_excerpt.rb

--- _plugins/custom_excerpt.rb
+++ _plugins/custom_excerpt.rb
@@ -1,9 +1,16 @@
-Jekyll::Hooks.register :documents, :pre_render do |doc|
-  if doc.content
-    # Remove all heading tags and other HTML
-    cleaned = doc.content.gsub(/<h[1-6][^>]*>.*?<\/h[1-6]>/im, '')
-    cleaned = cleaned.gsub(/<\/?[^>]*>/, '') # Remove all remaining HTML tags
-    cleaned = cleaned.strip.split(/\s+/)[0..49].join(' ') # Truncate to 50 words
+# _plugins/custom_excerpt.rb
+Jekyll::Hooks.register :documents, :post_render do |doc|
+  if doc.output
+    # Remove all heading tags (h1–h6)
+    cleaned = doc.output.gsub(/<h[1-6][^>]*>.*?<\/h[1-6]>/im, '')
+
+    # Remove all remaining HTML tags
+    cleaned = cleaned.gsub(/<\/?[^>]*>/, '')
+
+    # Normalize whitespace and truncate to 50 words
+    cleaned = cleaned.strip.split(/\s+/)[0..49].join(' ')
+
+    # Save cleaned excerpt
     doc.data['excerpt'] = cleaned
   end
 end
