a89be4974c064e8f94aa949f219c3bc072839457
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-09-11 22:53:43 -0400

    Create toc-bot.js

---
+++ assets/js/toc-bot.js
@@ -0,0 +1,14 @@
+import tocbot from "https://esm.sh/tocbot"
+
+tocbot.init({
+   tocSelector: '#toc-sidebar',     // container for TOC (your nav)
+ contentSelector: ".markdown",
+
+  headingSelector: 'h2, h3',        // headings you want included
+  collapseDepth: 6,                 // so sub-headings are nested
+  listClass: 'ml-4 space-y-1',           // apply your classes to <ul> Tocbot generates
+  itemClass: 'toc-link',                   // classes for <li> (add if needed)
+  linkClass: 'toc-link',    // classes for links
+  isCollapsedClass: 'hidden',      // classes for collapsed lists
+   activeLinkClass: 'active'
+});
