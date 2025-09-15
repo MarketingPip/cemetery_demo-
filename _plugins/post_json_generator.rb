147a66f9744f5c8f6e47f898dface6f0dbec4e77
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-09-15 03:12:42 -0400

    Update post_json_generator.rb

--- _plugins/post_json_generator.rb
+++ _plugins/post_json_generator.rb
@@ -1,22 +1,32 @@
+# _plugins/post_json_generator.rb
 module Jekyll
+  class PostJSONFile < StaticFile
+    def write(dest)
+      # Don't write file if it already exists (unless modified)
+      true
+    end
+  end
+
   class PostJSONGenerator < Generator
     safe true
     priority :low
 
     def generate(site)
       site.posts.docs.each do |post|
-        json = {
+        json_data = {
           title: post.data['title'],
-          date: post.date,
+          date: post.date.to_s,
           url: post.url,
           content: post.content
         }.to_json
 
-        # Path to write the JSON file
-        filename = File.join(site.dest, "api", "posts", "#{post.data['title'].downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')}.json")
+        filename = "#{post.data['title'].downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')}.json"
+        dir = File.join("api", "posts")
 
-        # Make directories if needed
-        FileUtils.mkdir_p(File.dirname(filename))
+        # Destination path
+        full_path = File.join(site.dest, dir)
+        FileUtils.mkdir_p(full_path)
+
         # Register it as a static file so Jekyll knows to include it
         site.static_files << PostJSONFile.new(site, site.dest, dir, filename)
       end
