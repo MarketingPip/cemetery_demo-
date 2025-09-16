e3534dd8c2a3b1ce926604a3b7608d455dd85467
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-09-15 22:21:55 -0400

    Update tag_json_generator.rb

--- _plugins/tag_json_generator.rb
+++ _plugins/tag_json_generator.rb
@@ -5,44 +5,71 @@ module Jekyll
     priority :low
 
     def generate(site)
-      # Get all unique tags from posts
-      all_tags = site.posts.docs.flat_map { |post| post.data['tags'] || [] }.uniq
+      # Build tag => [posts] hash
+      tag_map = Hash.new { |hash, key| hash[key] = [] }
 
-      all_tags.each do |tag|
-        # Get posts with this tag
-        tagged_posts = site.posts.docs.select do |post|
-          (post.data['tags'] || []).include?(tag)
+      site.posts.docs.each do |post|
+        Array(post.data['tags']).each do |tag|
+          tag_map[tag] << post
+        end
+      end
+
+      all_tags_json = []
+
+      tag_map.each do |tag, posts|
+        # Prepare the slug (URL-safe tag ID)
+        slug = tag.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+
+        # Collect minimal data per post
+        post_data = posts.map do |post|
+          {
+            title: post.data['title'],
+            date: post.date.to_s,
+            url: post.url,
+            excerpt: post.data['excerpt'] || post.content[0..100],
+            categories: post.data['categories'],
+            author: post.data['author']
+          }
         end
 
-        # Build JSON data
-        json = {
+        # Tag-specific JSON endpoint: /api/tags/:slug.json
+        tag_json = {
           id: tag,
-          posts: tagged_posts.map do |post|
-            {
-              title: post.data['title'],
-              date: post.date.to_s,
-              url: post.url,
-              excerpt: post.data['excerpt'] || post.content[0..100],
-              categories: post.data['categories'],
-              author: post.data['author']
-            }
-          end
+          count: posts.length,
+          posts: post_data
         }.to_json
 
-        # Create safe slug for filename
-        slug = tag.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-        path = "api/tags/#{slug}.json"
+        tag_path = "api/tags/#{slug}.json"
 
-        # Create virtual page
-        page = PageWithoutAFile.new(site, site.source, File.dirname(path), File.basename(path))
-        page.content = json
+        page = PageWithoutAFile.new(site, site.source, File.dirname(tag_path), File.basename(tag_path))
+        page.content = tag_json
         page.data['layout'] = nil
-        page.data['permalink'] = "/#{path}"
-        page.output = json
+        page.data['permalink'] = "/#{tag_path}"
+        page.output = tag_json
         page.ext = ".json"
 
         site.pages << page
+
+        # Add to tags index list
+        all_tags_json << {
+          id: tag,
+          slug: slug,
+          count: posts.length,
+          url: "/api/tags/#{slug}.json"
+        }
       end
+
+      # Now generate /api/tags.json
+      tags_index_json = all_tags_json.sort_by { |t| t[:id] }.to_json
+
+      index_page = PageWithoutAFile.new(site, site.source, "api", "tags.json")
+      index_page.content = tags_index_json
+      index_page.data['layout'] = nil
+      index_page.data['permalink'] = "/api/tags.json"
+      index_page.output = tags_index_json
+      index_page.ext = ".json"
+
+      site.pages << index_page
     end
   end
 end
