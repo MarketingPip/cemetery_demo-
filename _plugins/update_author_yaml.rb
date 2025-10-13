3cf1b8e40bd142a2513d78abf21a3ccb47051c70
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-10-13 18:02:39 -0400

    Update update_author_yaml.rb

--- _plugins/update_author_yaml.rb
+++ _plugins/update_author_yaml.rb
@@ -11,6 +11,7 @@ module Jekyll
       authors_dir = File.join(site.source, '_authors')
       site.collections['authors'] ||= Jekyll::Collection.new(site, 'authors')
 
+      # Handle blog posts
       site.posts.docs.each do |post|
         author_name = post.data['author']
         next unless author_name
@@ -25,7 +26,26 @@ module Jekyll
           'excerpt' => (post.data['excerpt'] || '').to_s[0, 100]
         }
 
-        update_author_file(site, authors_dir, author_name, post_info)
+        update_author_file(site, authors_dir, author_name, post_info, nil)
+      end
+
+      # Handle exhibits
+      if site.collections['exhibits']
+        site.collections['exhibits'].docs.each do |exhibit|
+          author_name = exhibit.data['author']
+          next unless author_name
+
+          slug = exhibit.data['slug'] || File.basename(exhibit.relative_path, '.md')
+
+          exhibit_info = {
+            'title' => exhibit.data['title'],
+            'url' => "/exhibits/#{slug}",
+            'date' => exhibit.data['date'].strftime('%Y-%m-%d') rescue nil,
+            'location' => exhibit.data['location'] || ''
+          }
+
+          update_author_file(site, authors_dir, author_name, nil, exhibit_info)
+        end
       end
 
       Jekyll.logger.info "Author YAML update complete."
@@ -33,7 +53,7 @@ module Jekyll
 
     private
 
-    def update_author_file(site, authors_dir, author_name, post_info)
+    def update_author_file(site, authors_dir, author_name, post_info = nil, exhibit_info = nil)
       author_filename = "#{author_name.downcase.strip.gsub(/\s+/, '-')}.md"
       author_path = File.join(authors_dir, author_filename)
 
@@ -41,19 +61,38 @@ module Jekyll
         content = File.read(author_path)
         parsed = parse_front_matter(content)
 
-        parsed[:data]['posts'] ||= []
-        existing_index = parsed[:data]['posts'].index { |p| p['url'] == post_info['url'] }
+        # Handle posts
+        if post_info
+          parsed[:data]['posts'] ||= []
+          existing_index = parsed[:data]['posts'].index { |p| p['url'] == post_info['url'] }
 
-        if existing_index
-          if parsed[:data]['posts'][existing_index] != post_info
-            parsed[:data]['posts'][existing_index] = post_info
-            Jekyll.logger.info "Updated existing post entry for author '#{author_name}': #{post_info['title']}"
+          if existing_index
+            if parsed[:data]['posts'][existing_index] != post_info
+              parsed[:data]['posts'][existing_index] = post_info
+              Jekyll.logger.info "Updated existing post entry for author '#{author_name}': #{post_info['title']}"
+            else
+              Jekyll.logger.info "No changes for post entry: #{post_info['title']}"
+            end
           else
-            Jekyll.logger.info "No changes for post entry: #{post_info['title']}"
+            parsed[:data]['posts'] << post_info
+            Jekyll.logger.info "Added new post entry for author '#{author_name}': #{post_info['title']}"
           end
-        else
-          parsed[:data]['posts'] << post_info
-          Jekyll.logger.info "Added new post entry for author '#{author_name}': #{post_info['title']}"
+        end
+
+        # Handle exhibits
+        if exhibit_info
+          parsed[:data]['exhibits'] ||= []
+          existing_index = parsed[:data]['exhibits'].index { |e| e['url'] == exhibit_info['url'] }
+
+          if existing_index
+            if parsed[:data]['exhibits'][existing_index] != exhibit_info
+              parsed[:data]['exhibits'][existing_index] = exhibit_info
+              Jekyll.logger.info "Updated exhibit for author '#{author_name}': #{exhibit_info['title']}"
+            end
+          else
+            parsed[:data]['exhibits'] << exhibit_info
+            Jekyll.logger.info "Added exhibit for author '#{author_name}': #{exhibit_info['title']}"
+          end
         end
 
         parsed_content = parsed[:content] || ''
@@ -61,12 +100,42 @@ module Jekyll
         new_content = "---\n#{yaml_content}---\n#{parsed_content}"
 
         File.write(author_path, new_content)
-        File.utime(Time.now, Time.now, author_path) # ensure mtime update
+        File.utime(Time.now, Time.now, author_path)
+
         Jekyll.logger.info "Wrote updated file: #{author_path}"
 
         update_site_data(site, author_filename, parsed[:data], parsed_content)
       else
-        create_author_file(site, author_path, author_name, post_info)
+        create_author_file(site, author_path, author_name, post_info, exhibit_info)
+      end
+    end
+
+    def create_author_file(site, author_path, author_name, post_info = nil, exhibit_info = nil)
+      data = {
+        'name' => author_name,
+        'posts' => post_info ? [post_info] : [],
+        'exhibits' => exhibit_info ? [exhibit_info] : []
+      }
+
+      yaml_content = data.to_yaml.sub(/^---\n/, '')
+      content = "---\n#{yaml_content}---\n"
+
+      File.write(author_path, content)
+      File.utime(Time.now, Time.now, author_path)
+
+      Jekyll.logger.info "Created new author file for: #{author_name} at #{author_path}"
+
+      new_doc = Jekyll::Document.new(
+        author_path,
+        { site: site, collection: site.collections['authors'] }
+      )
+      new_doc.read
+
+      unless site.collections['authors'].docs.any? { |d| d.path == new_doc.path }
+        site.collections['authors'].docs << new_doc
+        Jekyll.logger.info "Added new in-memory doc for #{author_name}"
+      else
+        Jekyll.logger.warn "Skipped adding duplicate doc for #{author_name}"
       end
     end
 
@@ -75,7 +144,6 @@ module Jekyll
       basename = File.basename(filename, '.md')
       author_path = File.join(site.source, '_authors', filename)
 
-      # Find existing doc by path or basename
       existing_doc = collection.docs.find do |d|
         d.path == author_path || d.basename_without_ext == basename
       end
@@ -93,40 +161,12 @@ module Jekyll
         new_doc.data.merge!(data)
         new_doc.content = content || ''
 
-        # Prevent duplicates before adding
         unless collection.docs.any? { |d| d.path == new_doc.path }
           collection.docs << new_doc
           Jekyll.logger.info "Added new in-memory doc for #{filename}"
         else
           Jekyll.logger.warn "Skipped duplicate doc for #{filename}"
         end
-      end
-    end
-
-    def create_author_file(site, author_path, author_name, post_info)
-      data = {
-        'name' => author_name,
-        'posts' => [post_info]
-      }
-      yaml_content = data.to_yaml.sub(/^---\n/, '')
-      content = "---\n#{yaml_content}---\n"
-      File.write(author_path, content)
-      File.utime(Time.now, Time.now, author_path)
-
-      Jekyll.logger.info "Created new author file for: #{author_name} at #{author_path}"
-
-      new_doc = Jekyll::Document.new(
-        author_path,
-        { site: site, collection: site.collections['authors'] }
-      )
-      new_doc.read
-
-      # Add to collection only if not already present
-      unless site.collections['authors'].docs.any? { |d| d.path == new_doc.path }
-        site.collections['authors'].docs << new_doc
-        Jekyll.logger.info "Added new in-memory doc for #{author_name}"
-      else
-        Jekyll.logger.warn "Skipped adding duplicate doc for #{author_name}"
       end
     end
 
