473dddc9f75c144226b0e8b223af3ae4b31cfa2a
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-10-16 20:58:43 -0400

    Refactor tag page generation to include categories

--- _plugins/tag_and_cats.rb
+++ _plugins/tag_and_cats.rb
@@ -1,56 +1,71 @@
-# Filename: _plugins/generate_tag_pages.rb
 module Jekyll
-  class GenerateTagPages < Generator
+  class GenerateTagAndCategoryPages < Generator
     safe true
     priority :low
 
-    # Configure how many posts per page
+    # Default posts per page
     POSTS_PER_PAGE = 10
 
+    COLLECTIONS = %w[blog exhibits].freeze
+    KEYS = %w[categories tags].freeze
+
     def generate(site)
-      Jekyll.logger.info "Starting tag page generation with pagination..."
+      COLLECTIONS.each do |collection_name|
+        collection = site.collections[collection_name]
+        next unless collection
 
-      # Collect all unique tags
-      all_tags = site.posts.docs.flat_map { |p| p.data['tags'] || [] }.uniq
-      Jekyll.logger.info "Found tags: #{all_tags.join(', ')}"
+        docs = collection.docs.sort_by { |d| d.date || Time.at(0) }.reverse
+        next if docs.empty?
 
-      all_tags.each do |tag|
-        tagged_posts = site.posts.docs.select { |p| (p.data['tags'] || []).include?(tag) }
-        tagged_posts.sort_by! { |p| -p.date.to_f }
+        KEYS.each do |key|
+          terms = collect_terms(docs, key)
+          terms.each do |term|
+            term_docs = docs.select { |d| Array(d.data[key] || []).map(&:to_s).include?(term) }
+            total_pages = (term_docs.size.to_f / POSTS_PER_PAGE).ceil
+            next if total_pages.zero?
 
-        total_pages = (tagged_posts.size.to_f / POSTS_PER_PAGE).ceil
-        Jekyll.logger.info "Generating #{total_pages} pages for tag '#{tag}'"
-
-        (1..total_pages).each do |page_num|
-          paginate(site, tag, tagged_posts, page_num, total_pages)
+            (1..total_pages).each do |page_num|
+              paginate(site, collection_name, key, term, term_docs, page_num, total_pages)
+            end
+          end
         end
       end
-
-      Jekyll.logger.info "Tag page generation complete."
+      Jekyll.logger.info "Pagination:", "Tag and category pages generation complete."
     end
 
-    def paginate(site, tag, tagged_posts, page_num, total_pages)
-      # Slice posts for this page
+    private
+
+    def collect_terms(docs, key)
+      docs.flat_map { |d| Array(d.data[key] || []) }.map(&:to_s).uniq.compact
+    end
+
+    def paginate(site, collection_name, key, term, term_docs, page_num, total_pages)
       start_index = (page_num - 1) * POSTS_PER_PAGE
-      end_index = start_index + POSTS_PER_PAGE - 1
-      page_posts = tagged_posts[start_index..end_index] || []
+      page_posts = term_docs[start_index, POSTS_PER_PAGE] || []
 
-      dir = page_num == 1 ? "blog/tag/#{tag}" : "blog/tag/#{tag}/page#{page_num}"
+      # slugify term
+      slug = term.to_s.downcase.strip.gsub(' ', '-').gsub(/[^a-z0-9\-]/, '')
+
+      dir = if page_num == 1
+              "#{collection_name}/#{key}/#{slug}"
+            else
+              "#{collection_name}/#{key}/#{slug}/page#{page_num}"
+            end
+
       name = "index.html"
-
       Jekyll.logger.info "Creating #{dir}/#{name}"
 
       begin
         page = Jekyll::PageWithoutAFile.new(site, site.source, dir, name)
         page.data = {
-          'layout' => 'tag',
-          'tag' => tag,
+          'layout' => key.chomp('s'), # 'category' or 'tag'
+          key.chomp('s') => term,
           'robots' => 'noindex',
           'paginator' => {
             'page' => page_num,
             'per_page' => POSTS_PER_PAGE,
             'total_pages' => total_pages,
-            'total_posts' => tagged_posts.size,
+            'total_posts' => term_docs.size,
             'previous_page' => (page_num > 1 ? page_num - 1 : nil),
             'next_page' => (page_num < total_pages ? page_num + 1 : nil),
             'posts' => page_posts
@@ -59,7 +74,7 @@ module Jekyll
         page.content = ''
         site.pages << page
       rescue StandardError => e
-        Jekyll.logger.error "Error creating paginated page for tag '#{tag}': #{e.message}"
+        Jekyll.logger.error "Error creating page for #{collection_name}/#{key}/#{term}: #{e.message}"
         raise
       end
     end
