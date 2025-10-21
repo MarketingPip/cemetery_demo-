0da0364437b3ad919fd2d97f0b2c1f84c40569fa
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-10-20 20:34:57 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// ./tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./_drafts/**/*.md",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_pages/*.{html,md}",
+    "./_posts/*.md",
+    "./*.{html,md}",
+  ],
+  // ...
+};
