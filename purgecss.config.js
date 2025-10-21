b39f3f26ae96329b37afc6bbcea5ddaa25b252a3
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-10-21 02:11:34 -0400

    Delete purgecss.config.js

--- purgecss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  content: ["_site/**/*.html", "_site/**/*.js"],
-  css: ["_site/assets/css/*.css", "!_site/assets/css/tailwind.css"],
-  output: "_site/assets/css/",
-  skippedContentGlobs: ["_site/assets/**/*.html"],
-};
