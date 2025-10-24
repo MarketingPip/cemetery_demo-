e49d66c1a9d143b6674781aa5250496868971b75
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-10-24 19:58:07 -0400

    Update add-authors.js

--- plugins/add-authors.js
+++ plugins/add-authors.js
@@ -286,7 +286,7 @@ export const plugin = {
 
       try {
 
-        const octokit = pluginContext.getOctokit
+        const { octokit, config } = pluginContext.getOctokit
 
         const { data } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
           owner: config.owner,
@@ -396,7 +396,7 @@ export const plugin = {
     // Edit author
     async function editAuthor(path, sha, pluginContext) {
       try {
-        const octokit = pluginContext.getOctokit
+        const { octokit, config } = pluginContext.getOctokit
 
         const { data } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
           owner: config.owner,
@@ -436,7 +436,7 @@ export const plugin = {
       }
 
       try {
-        const octokit = pluginContext.getOctokit;
+        const { octokit, config } = pluginContext.getOctokit;
 
         await octokit.request('DELETE /repos/{owner}/{repo}/contents/{path}', {
           owner: config.owner,
@@ -488,7 +488,7 @@ export const plugin = {
       const path = `_authors/${filename}`;
 
       try {
-        const octokit = pluginContext.getOctokit
+        const { octokit, config } = pluginContext.getOctokit
 
         let sha = currentEditAuthor?.sha;
 
