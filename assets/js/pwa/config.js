e87419fa849a34ce2f63d2dd6892cd992320d0d7
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-10-24 01:40:35 -0400

    Update config.js

--- assets/js/pwa/config.js
+++ assets/js/pwa/config.js
@@ -152,7 +152,7 @@ const PWA_CONFIG = {
     // Precache config
     precache: {
         // Enable/disable precaching
-        active: false,
+        active: true,
         // Routes to
         routes: [
        //     '/page1.html',
