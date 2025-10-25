edb7ad9cae58ec76d63470e59cf208a19516b657
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-10-24 20:48:26 -0400

    Update import-test.js

--- plugins/import-test.js
+++ plugins/import-test.js
@@ -1,3 +1,3 @@
-import {plugin} from "https://esm.sh/gh/MarketingPip/cemetery_demo-@main/plugins/add-authors.js"
+import * as _plugin from "https://esm.sh/gh/MarketingPip/cemetery_demo-@main/plugins/add-authors.js"
 
-export plugin
+const plugin = _plugin
