b21726be9823510dd3d42486edd87c6ce8b11239
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-10-25 01:03:08 -0400

    Create default_Example.js

---
+++ plugins/default_Example.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    name: "Default Plugin 1",
+    init: (context) => { /* Initialization code */ }
+  },
+  {
+    name: "Default Plugin 2",
+    init: (context) => { /* Initialization code */ }
+  }
+];
